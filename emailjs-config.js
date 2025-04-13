9753acce055360ce03b9d2a4c94775c8b27e22eb
codinghubindia <codinghubindia@users.noreply.github.com>
2025-04-13 11:23:23 +0000

    deploy: 951ca80b874653c39d71f3467da9fa8dec1490e8

---
+++ emailjs-config.js
@@ -0,0 +1,6 @@
+
+// EmailJS configuration file - created at build time
+window.VITE_EMAILJS_SERVICE_ID = "${VITE_EMAILJS_SERVICE_ID}";
+window.VITE_EMAILJS_TEMPLATE_ID = "${VITE_EMAILJS_TEMPLATE_ID}";
+window.VITE_EMAILJS_PUBLIC_KEY = "${VITE_EMAILJS_PUBLIC_KEY}";
+console.log("EmailJS config loaded from separate file");
